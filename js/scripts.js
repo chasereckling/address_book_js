b76ec21e6017e98fd579c9a334ffd63581ea43bc
Chase <chase.reckling@gmail.com>
2015-06-02 16:52:53 -0700

    add type of address to form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
+function Contact(firstName,lastName){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.addresses = [];
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+function Address(types, street, city, state) {
+  this.types = types;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.types + ", " + this.street + ", " + this.city + ", " + this.state;
+}
+
+function resetFields() {
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+    $("input.new-types").val("");
+
+}
+
 $(document).ready(function(){
   $("#add-address").click(function() {
     $("#new-addresses").append('<div class="new-address">' +
@@ -13,6 +44,10 @@ $(document).ready(function(){
     '<label for="new-state">State</label>' +
     '<input type="text" class="form-control new-state">' +
     '</div>' +
+    '<div class="form-group">' +
+    '<label for="new-types">Address Type</label>' +
+    '<input type="text" class="form-control new-types">' +
+    '</div>' +
     '</div>');
   });
 
@@ -21,37 +56,33 @@ $(document).ready(function(){
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, addresses: []
-  };
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
   $(".new-address").each(function() {
     var inputtedStreet = $(this).find("input.new-street").val();
     var inputtedCity = $(this).find("input.new-city").val();
     var inputtedState = $(this).find("input.new-state").val();
+    var inputtedTypes = $(this).find("input.new-types").val();
 
-    var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
+
+    var newAddress = {  types: inputtedTypes, street: inputtedStreet, city: inputtedCity, state: inputtedState};
     newContact.addresses.push(newAddress);
   });
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
-
+  $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
   $(".contact").last().click(function() {
-    $("#show-contact").show();
+    $("#show-contact").fadeIn();
     $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
     $(".first-name").text(newContact.firstName);
     $(".last-name").text(newContact.lastName);
-
     $("ul#addresses").text("");
+
     newContact.addresses.forEach(function(address) {
-      $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>");
+      $("ul#addresses").append("<p>"+ address.types + ": " + address.street + ", " + address.city + ", " + address.state  + "</p>");
     });
   });
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
+  resetFields();
   });
 });
